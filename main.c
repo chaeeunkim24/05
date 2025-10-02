09e887292fff9273965f075a133407e1196d4f33
chaeeunkim24 <chae020703@sookmyung.ac.kr>
2025-10-02 15:50:38 +0900

    practice_4

--- main.c
+++ main.c
@@ -3,18 +3,23 @@
 
 int main(int argc, char *argv[])
 {
-    int x;
-    int i;
-    int sum = 0;
-    
-    printf("input a number: ");
-    scanf("%i", &x);
-    
-    for (i=1 ; i <= x ; i++)
-        sum = sum + i;
-    
-    printf("the result is %i\n", sum);
-        
+    int x, y;
+    char op;
+
+    printf("input x, op, y: ");
+    scanf("%i %c %i", &x, &op, &y);
+
+    if (op == '+')
+        printf("%i %c %i = %i\n", x, op, y, x+y);
+    else if (op == '-')
+        printf("%i %c %i = %i\n", x, op, y, x-y);
+    else if (op == '*')
+        printf("%i %c %i = %i\n", x, op, y, x*y);
+    else if (op == '/')
+        printf("%i %c %i = %i\n", x, op, y, x/y);
+    else
+        return 0;
+
     system("PAUSE");	
     return 0;
 }
